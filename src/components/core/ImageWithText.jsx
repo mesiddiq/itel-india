fdae7f94b93e2c81df017ad7292d52016afc64e4
Aashish Gulshan <78717816+aashishgulshan@users.noreply.github.com>
2024-10-08 13:11:17 +0530

    Added responsive

--- src/components/core/ImageWithText.jsx
+++ src/components/core/ImageWithText.jsx
@@ -3,8 +3,8 @@ import Button from '../common/Button';
 import image1 from '../../assets/image1.png';
 const ImageWithText = () => {
     return (
-        <div className='grid grid-cols-2 items-center justify-center max-w-7xl mx-auto'>
-            <div className='px-4 lg:px-12'>
+        <div className='flex flex-col lg:flex-row items-center justify-center max-w-7xl mx-auto'>
+            <div className='px-8 lg:px-12'>
                 <p className='text-3xl font-bold mb-4'>
                     Transform Every Moment with itel's Family-Friendly Smart TVs!
                 </p>
@@ -13,7 +13,7 @@ const ImageWithText = () => {
                 <Button btnLabel="Explore TV" />
             </div>
             <div className='flex justify-center items-center'>
-                <img src={image1} alt="kjgn" className='w-full h-full object-cover' />
+                <img src={image1} alt="image1" className='w-full h-full object-cover' />
             </div>
         </div>
     );
