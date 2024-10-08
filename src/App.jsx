cb842f542669982c8f9f1ba08b9f5993073a0315
28vikas <63vikas@gmail.com>
2024-10-08 14:15:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from './pages/HomePage'
 function App() {
 
 	return (
-		<main className="bg-[#111111] min-h-screen space-y-[60px] md:space-y-[120px] px-5 md:px-0">
+		<main className="bg-[#111111] min-h-screen space-y-[60px] w-[1441px] md:space-y-[120px] px-5 md:px-0">
 			<HomePage />
 		</main>
 	)
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HotPicks from '../components/core/HotPicks'
 
 const HomePage = () => {
     return (
         <div className='content'>
             <h1 className='text-white'>Home Page</h1>
+            <HotPicks/>
         </div>
     )
 }
