5fb437d600d0d9ac58b196bf922676e992edf324
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-10-21 07:54:05 +0530

    Completed Sections are as follows:- HeroSections, Feature Rich page, Marquee and Video Sections, Image Caraousel sections, and the Full Specifications one

--- src/components/common/smartphone_common/BuyNowSquareBG.jsx
+++ src/components/common/smartphone_common/BuyNowSquareBG.jsx
@@ -2,7 +2,7 @@ import BuyNowText from "./BuyNowText"
 
 const BuyNowSquareBG = () => {
   return (
-    <div className="bg-BuyNowSquareBG bg-contain bg-no-repeat relative w-[160px] h-[48px] flex flex-col justify-center items-center">
+    <div className="bg-[url('/SmartPhone/S24/ButtonBackground.png')] bg-contain bg-no-repeat relative w-[160px] h[48px] md:flex flex-col justify-center items-center hidden">
         <BuyNowText content="Buy Now" className=""/>
     </div>
   )
---
+++ src/data/smartphonedata/A05Data.js
@@ -0,0 +1,180 @@
+export const a05FeatureRichData = {
+    title: "Store More, Worry Less with Massive 64GB ROM",
+    description: "Experience breathtaking visuals on the itel A05s smartphone with its expansive 6.6-inch HD+ Drop Notch Display, featuring a crisp 1612 x 720 pixel resolution. Stay powered all day with the robust 4000 mAh battery and Type-C charging support. Effortlessly multitask with 8GB* (4GB+4GB*) RAM and 64GB ROM. Capture stunning photos with the 5MP dual rear camera and enjoy enhanced security with dual protection for your device.",
+    cards: [
+        {
+            id: 1,
+            title: "Blazing Speed with 8GB*(4GB+4GB*) RAM & 64GB ROM",
+            description: "Ensure smooth multitasking and seamless app navigation with 8GB* (4GB+4GB*) RAM using memory fusion technology, combined with ample 64GB ROM.",
+            icon: "/SmartPhone/a05/logos/logo1.png",
+        },
+        {
+            id: 2,
+            title: "Big Screen Brilliance: 6.6” HD+ Drop Notch Display",
+            description: "Immerse yourself in every detail with vibrant colours and sharp clarity in our 6.6” HD+ drop notch display.",
+            icon: "/SmartPhone/a05/logos/logo2.png",
+        },
+        {
+            id: 3,
+            title: "Turbocharge Your Day: 4000 mAh Battery & Type-C Charging",
+            description: "No more battery anxiety! With lightning-fast Type-C charging & powerful 4000 mAh battery keeps you going non-stop.",
+            icon: "/SmartPhone/a05/logos/logo3.png",
+        }
+    ]
+};
+
+
+export const a05ImageCaraousel = [
+    {
+        id: 1,
+        color: "orange",
+        buttonText: "Orange",
+        buttonGradient: "bg-[#FDAB47]",
+        colorSelectorBGColor: "",
+        colorSelectorTextColor: "text-black",
+        bgContainer: "bg-[#FDAB47]",
+        bgURL: '/SmartPhone/a05/galleryBG/orange.png',
+        images: [
+            "/SmartPhone/a05/products/Orange1.webp",
+            "/SmartPhone/a05/products/Orange2.webp",
+        ],
+    },
+    {
+        id: 2,
+        color: "black",
+        buttonText: "Black",
+        buttonGradient: "bg-[#475266]",
+        colorSelectorBGColor: "",
+        colorSelectorTextColor: "text-black",
+        bgContainer: "bg-[#475266]",
+        bgURL: '/SmartPhone/a05/galleryBG/white.png',
+        images: [
+            "/SmartPhone/a05/products/Starry Black 1.webp",
+            "/SmartPhone/a05/products/Starry Black 2.webp",
+            "/SmartPhone/a05/products/Starry Black 3.webp",
+        ]
+    },
+    {
+        id: 3,
+        color: "green",
+        buttonText: "Green",
+        buttonGradient: "bg-[#CDE4DA]",
+        colorSelectorBGColor: "",
+        colorSelectorTextColor: "text-black",
+        bgContainer: "bg-[#CDE4DA]",
+        bgURL: '/SmartPhone/a05/galleryBG/green.png',
+        images: [
+            "/SmartPhone/a05/products/green1.webp",
+            "/SmartPhone/a05/products/green2.webp",
+        ]
+    },
+    {
+        id: 4,
+        color: "blue",
+        buttonText: "Blue",
+        buttonGradient: "bg-[#96CAE5]",
+        colorSelectorBGColor: "",
+        colorSelectorTextColor: "text-black",
+        bgContainer: "bg-[#96CAE5]",
+        bgURL: '/SmartPhone/a05/galleryBG/blue.png',
+        images: [
+            "/SmartPhone/a05/products/blue1.webp",
+            "/SmartPhone/a05/products/blue2.webp",
+        ]
+    },
+];
+
+export const a05FullSpecifications = [
+    {
+        id:1,
+        label:'General',
+        data:[
+            {label:'Model',value:'A05s'},
+            {label:'Colors',value:'Crystal Blue, Glorious Orange, Meadow Green & Nebula Black'},
+
+        ]
+    },
+    {
+        id:2,
+        label:'Display Features',
+        data:[
+            {label:'Display Size',value:'6.56 inch'},
+            {label:'Resolution',value:'1612x720 Pixels'},
+            {label:'Resolution Type',value:'HD + IPS'},
+            {label:'Dimension',value:'Dimension'},
+            {label:'Technology',value:'G+F, Incell'},
+            {label:'Refresh Rate',value:'60 Hz'},
+            {label:'Touch Sampling Rate',value:'120 Hz'},
+            {label:'Pixel Density',value:'270 PPI'},
+        ]
+    },
+    {
+        id:3,
+        label:'OS & Processor',
+        data:[
+            {label:'Operating System',value:'Android 13 Go Edition'},
+            {label:'Chipset',value:'UNISOC SC9863A'},
+            {label:'Processor Core',value:'Octa-Core'},
+        ]
+    },
+    {
+        id:4,
+        label:'Camera',
+        data:[
+            {label:'Rear Camera',value:'8MP'},
+            {label:'Rear flash',value:'Yes'},
+            {label:'Front Camera',value:'5MP'},
+            {label:'Front flash',value:'No'},
+        ]
+    },
+    {
+        id:5,
+        label:'Battery',
+        data:[
+            {label:'Battery Capacity',value:'4000 mAh'},
+            {label:'Battery Type',value:'Li-Polymer'},
+            {label:'Fast Charging',value:'No'},
+            {label:'USB',value:'Type-C'},
+            {label:'Standby',value:'32 Days'},
+            {label:'Talktime',value:'7.5 Hours'},
+            {label:'',value:'112 Hours'},
+        ]
+    },
+    {
+        id:6,
+        label:'Memory & Storage',
+        data:[
+            {label:'RAM',value:'8GB* (4GB + 4GB*)'},
+            {label:'ROM',value:'64 GB'},
+            {label:'User Memory',value:'53 GB'},
+            {label:'Expandable',value:'UPTO 1 TB'},
+        ]
+    },
+    {
+        id:7,
+        label:'Connectivity',
+        data:[
+            {label:'Network Type',value:'4G'},
+            {label:'GSM',value:'2G:B3, 5, 8'},
+            {label:'EDGE',value:'Support Download'},
+            {label:'WCDMA',value:'3G:B1, 5, 8'},
+            {label:'LTE',value:'B1/B3/B5/B8/B40/B38/B41(120M)'},
+            {label:'SIM Slot',value:'Slot1 & Slot2-Nano'},
+            {label:'Dual Active/Standby',value:'Dual Active'},
+            {label:'Bluetooth Support',value:'Yes'},
+            {label:'Wi-Fi',value:'Yes'},
+            {label:'GPS',value:'Yes'},
+            {label:'USB Connectivity',value:'Yes'},
+        ]
+    },
+    {
+        id:8,
+        label:'Additional',
+        data:[
+            {label:'Earphone Jack',value:'3.5mm'},
+            {label:'Sensors',value:'Fingerprint | Face Unlock | G Sensor'},
+            {label:'Fingerprint',value:'Side'},
+            {label:'Accessories',value:'5W Adapter(support 10W) | USB Cable | Protective Cover'},
+        ]
+    },
+]
