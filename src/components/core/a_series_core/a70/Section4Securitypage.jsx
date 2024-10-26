9abedabd3b87ff7442e93ea9af36402ca0db9ad3
Prakash-b1 <pinnub23@gmail.com>
2024-10-26 23:07:17 +0530

    added padding and spaces

--- src/components/core/a_series_core/a70/Section4Securitypage.jsx
+++ src/components/core/a_series_core/a70/Section4Securitypage.jsx
@@ -10,12 +10,12 @@ const Section4Securitypage = () => {
           className="xl:absolute top-[20rem] xl:h-[739px] xl:max-w-[1248px] md:top-0 left-0 w-full object-cover"
         />
         <div className="z-10 flex xl:justify-end justify-center text-center md:text-left md:items-start md:px-[69px] lg:py-[50px] px-[13px] py-[11px]">
-          <div className="xl:w-[401px]">
+          <div className="xl:w-[401px] flex flex-col gap-4">
             <p className="text-desktop/h2 md:block hidden xl:max-w-[458px]">
               Hello! <br /> Hassle-free Security
             </p>
             <p className="text-mobile/h4 md:hidden">Hello Hassle-free Security</p>
-            <p className="md:text-mobile/body/large text-mobile/body/2">
+            <p className="md:text-mobile/body/large w-[295px] lg:w-[401px] md:pt-5 lg:pt-0 md:w-full text-mobile/body/2">
               With itel A70s side-mounted fingerprint sensor, experience seamless login, swift access, and top-notch security.
             </p>
           </div>
