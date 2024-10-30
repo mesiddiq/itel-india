dca34da39ef0e1dd4579ae232868d06e72c7473b
aryanploxxx <aryangupta8291@gmail.com>
2024-10-30 18:25:02 +0530

    fix aura 05i

--- src/pages/a_series/A05i.jsx
+++ src/pages/a_series/A05i.jsx
@@ -1,18 +1,19 @@
 import FullSpecifications from "../../components/common/FullSpecifications"
-import ColorImageCarousel from "../../components/common/smartphone_common/ColorImageCarousel"
 import HeroSmartPhoneCommon from "../../components/common/smartphone_common/HeroSmartPhoneCommon"
 import SliderAnimationCommon from "../../components/common/smartphone_common/SliderAnimationCommon"
 import StickyHeader from "../../components/common/StickyHeader"
-import FeatureSectionA05i from "../../components/core/a_series_core/A05i/FeatureSectionA05i"
+import FeatureSectionA05i from "../../components/core/a_series_core/A05i/FeatureSectionA05I"
+import ColorImageCarousel from "../../components/core/a_series_core/A05i/ColorImageCarousel"
 import MultipleCardComponents from "../../components/core/a_series_core/A05i/MultipleCardComponents"
 import Parent4Divs from "../../components/core/a_series_core/A05i/Parent4Divs"
 import TimelessLeatherElegance from "../../components/core/a_series_core/A05i/TimelessLeatherElegance"
+
 import { FullSpecificationsA05i } from '../../data/smartphonedata/A05iData'
 import { ImageCarouselData } from '../../data/smartphonedata/A05iImageCarouselData'
 
 const A05i = () => {
   return (
-    <div>
+    <>
 
         <HeroSmartPhoneCommon
           headingText="Aura 05i"
@@ -40,20 +41,23 @@ const A05i = () => {
           textGradient2="#6B6682"
         />
 
-      <Parent4Divs />
-      <TimelessLeatherElegance />
-      <MultipleCardComponents />
-      <div className="lg:px-0 py-[48px] lg:py-[120px] bg-white">
-        <div className="lg:content bg-white overflow-hidden">
-          <ColorImageCarousel ImageCarouselData={ImageCarouselData} showTitleInMobile={false} />
+        <Parent4Divs />
+
+        <TimelessLeatherElegance />
+
+        <MultipleCardComponents />
+
+        <div className="lg:px-0 py-[48px] lg:py-[120px] bg-white">
+            <div className="lg:content bg-white overflow-hidden">
+                <ColorImageCarousel ImageCarouselData={ImageCarouselData} showTitleInMobile={false}/>
+            </div>
         </div>
-      </div>
 
-      <div className="px-4 lg:pb-[80px] bg-white">
-        <FullSpecifications FullSpecificationsData={FullSpecificationsA05i} mode="light" gifStar="/smartphone/A05i/StarFullSpecifications.svg" />
-      </div>
+        <div className="px-4 lg:pb-[80px] bg-white">
+          <FullSpecifications FullSpecificationsData={FullSpecificationsA05i} mode="light" gifStar="/smartphone/A05i/StarFullSpecifications.svg"/>
+        </div>
 
-    </div>
+    </>
   )
 }
 
