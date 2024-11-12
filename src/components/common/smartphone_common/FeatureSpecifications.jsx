4975695e769f48774a0d17760eb3a355a63c7e32
Prakash-b1 <pinnub23@gmail.com>
2024-11-12 12:08:44 +0530

    add

--- src/components/common/smartphone_common/FeatureSpecifications.jsx
+++ src/components/common/smartphone_common/FeatureSpecifications.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable react/prop-types */
 const FeatureSpecifications = ({featureMetric, featureMetricDescription}) => {
   return (
-<<<<<<< HEAD
-    <div className='flex flex-col gap-[4px] lg:gap-2 justify-center items-center '>
-=======
     <div className='flex flex-col gap-[4px] lg:gap-2 items-center lg:items-start '>
->>>>>>> 4a11beb4c9df640cb8cec12d2ad2ed14ea70824b
         <span className='font-markot opacity-80 text-mobile/h6 lg:text-desktop/h3'>
             {featureMetric}
         </span>
