1b121a65cf9fd6d9944d0c1b42115e95665912c6
Nitish <nitishgautam710@gmail.com>
2024-11-18 23:57:33 +0530

    aboutus

--- src/components/core/static_pages_core/Aboutus/Bottem.jsx
+++ src/components/core/static_pages_core/Aboutus/Bottem.jsx
@@ -1,58 +1,39 @@
-// import React from "react";
-
-// const Bottem = () => {
-//   return (
-//     <div className=" content px-4">
-//       <div className='lg:bg-[url("/static_page/Aboutus/LogoExplainationBG.webp")] px-8 flex items-center  lg:pl-[79px] font-markot bg-[url("/static_page/Aboutus/LogoExplainationBGMobile.webp")] bg-cover  bg-center bg-no-repeat lg:min-h-[478px]    min-h-[840px]'>
-//         <div className="">
-//           <div className=" flex lg:flex-col-reverse   flex-col lg:items-start   items-center">
-//             <p className=" lg:w-[784px] text-black/1   lg:text-desktop/body/large ">
-//               Our logo "Stylized Speech Bubble" shape is not just a design
-//               element; it embodies our commitment to empathetic communication.
-//             </p>
-//             <div className=" ">
-//                 <img
-//               className=" w-[215px] lg:hidden h-[169px]"
-//               src="/static_page/Aboutus/Group1973340143(1).png"
-//               alt=""
-//             />
-//             <img
-//               className=" hidden lg:block"
-//               src="/static_page/Aboutus/Group1973340143.png"
-//               alt=""
-//             />
-//             </div>
-
-//           </div>
-//           <p className="lg:w-[754px]  text-grey/grey/4 lg:text-desktop/body/1">
-//             By integrating this shape into an abstract human face, we elevate
-//             its significance. This design enables deeper and more empathetic
-//             connections with users.
-//           </p>
-//           <p className="lg:w-[734px]  text-grey/grey/4 lg:text-desktop/body/1">
-//             Fluid waves (design element) symbolizes seamless connectivity and
-//             dynamic interactions, reflecting the intuitive flow of data and
-//             communication. Highlighting itel's mission to smoothly integrate
-//             affordable and smart solutions for everyone.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Bottem;
 import React from "react";
 
 const Bottem = () => {
   return (
-    <div className="content lg:pl-[107px] lg:py-[29px] relative md:h-[478px] px-4">
-    
-    <div>
-      
-    </div>
+    <div className="lg:content px-4">
+      <div className=" lg:h-[478px] overflow-hidden max-h-[730px] flex justify-between bg-[#f8f6f3] border-[#ebe8e2]    rounded-3xl lg:rounded-lg border-[24px] ">
+        <div className=" flex flex-col justify-between   lg:pl-[60px]  p-3  h-full py-[20px] lg:max-w-[784px]">
+          
+          <div className="  flex flex-col-reverse  lg:space-y-3 lg:flex-col ">
+          <img className=" self-center lg:hidden max-h-[169px] max-w-[215px]" src="/static_page/Aboutus/Group1973340143(1).png" alt="" />
+             
+             <img className=" hidden lg:block lg:h-[115px] lg:w-[90%]" src="/static_page/Aboutus/Group1973340143.png" alt="" />
+          <p className="  text-desktop/body/1   lg:text-desktop/body/large text-black/1">
+            Our logo "Stylized Speech Bubble" shape is not just a design
+            element; it embodies our commitment to empathetic communication.
+          </p>
+          </div>
+          
+         
 
-
+          <div className=" lg:space-y-12 space-y-3 text-desktop/body/1 text-grey/grey/4">
+            <p className=" lg:max-w-[750px]">
+              By integrating this shape into an abstract human face, we elevate
+              its significance. This design enables deeper and more empathetic
+              connections with users.
+            </p>
+            <p className="  lg:max-w-[737px]">
+              Fluid waves (design element) symbolizes seamless connectivity and
+              dynamic interactions, reflecting the intuitive flow of data and
+              communication. Highlighting itel's mission to smoothly integrate
+              affordable and smart solutions for everyone.
+            </p>
+          </div>
+        </div>
+        <img className=" hidden lg:block h-full" src="/static_page/Aboutus/pattern-2.png" alt="" />
+      </div>
     </div>
   );
 };
