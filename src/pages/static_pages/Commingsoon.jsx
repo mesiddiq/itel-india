8a857df65399fa85387611a5b4a75e5051e2f133
nitish <nitishgautam710@gmail.com>
2024-11-28 12:20:25 +0530

    changes done

--- src/pages/static_pages/Commingsoon.jsx
+++ src/pages/static_pages/Commingsoon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Commingsoon = () => {
   return (
-    <div className=' lg:bg-[url("/static_page/commingsoon/commingsoon.png")]    bg-[url("/static_page/commingsoon/commingsoonmo.png")]   h-[400px] lg:h-[690px] bg-cover bg-center bg-no-repeat'>
+    <div className=' lg:bg-[url("/static_page/commingsoon/commingsoon.webp")]  overflow-x-hidden    bg-[url("/static_page/commingsoon/commingsoonmo.webp")]   h-[350px] lg:h-[690px] bg-[#262626] bg-contain lg:bg-cover bg-center bg-no-repeat'>
        
     </div>
   )
