750787e119ed99c57bfa2802a98ece3cecfed288
Sahil Kumar <sahilkumarofficial05@gmail.com>
2025-03-23 13:08:39 +0530

    Added king singla section

---
+++ src/data/featurephonedata/KingSignal.js
@@ -0,0 +1,113 @@
+export const HeroDataKingSignal = {
+    title: "King Signal",
+    subtitle: 'Unique Design with Round Screen | Inbuilt BT Caller | Lanyard to Carry Around',
+    bgDesktop: '/featurephone/Circle_1/Backgrounds/banner.webp',
+    bgMobile: '/featurephone/Circle_1/Backgrounds/bannermobile.webp',
+    purchaseLink: 'https://amzn.in/d/5QhNgaP',
+    icon: "/featurephone/superguru4g/buynowlogo.svg",
+    phoneUrl: '/featurephone/Circle_1/Backgrounds/hero.webp',
+}
+
+export const FullSpecificationsDataKingSignal = [
+    {
+        id: 1,
+        label: 'General',
+        data: [
+            { label: 'Model', value: 'Circle 1' },
+            { label: 'Colors', value: 'Rose Gold & Black' },
+            { label: 'Display', value: '3.35 cm (1.32")' },
+            { label: 'Battery', value: '500 mAh' },
+            { label: 'Language Support', value: '9 (English, Hindi, Gujarati, Tamil, Telugu, Bengali, Kannada & Malayalam & Punjabi)' },
+            { label: 'Phonebook', value: '500' },
+            { label: 'SMS', value: '200' },
+            { label: 'Message', value: 'Yes' },
+            { label: 'Torch', value: 'Yes' },
+        ]
+    },
+    {
+        id: 2,
+        label: 'Display Features',
+        data: [
+            { label: 'Display Size', value: '3.35 cm (1.32") 2"' },
+            { label: 'Resolution', value: '128*128' },
+            { label: 'Resolution Type', value: 'QVGA' },
+            { label: 'Pixel Density', value: '137 ppi' },
+        ]
+    },
+    {
+        id: 3,
+        label: 'Battery',
+        data: [
+            { label: 'Battery Capacity', value: '500 mAh' },
+            { label: 'Battery Type', value: 'Li-ion' },
+            { label: 'Battery Backup', value: '212.7 Hours' },
+            { label: 'Talktime', value: '7.75 Hours' },
+        ]
+    },
+    {
+        id: 4,
+        label: 'Camera',
+        data: [
+            { label: 'Rear Camera', value: '0.08M' },
+            { label: 'Rear flash', value: 'Yes' },
+        ]
+    },
+    {
+        id: 5,
+        label: 'Memory & Storage',
+        data: [
+            { label: 'ROM', value: '4 MB' },
+            { label: 'RAM', value: '4 MB' },
+            { label: 'Chipset', value: 'SC6513E' },
+            { label: 'Dimension', value: '97.6 * 49 * 11.2mm' },
+            { label: 'Expandable Memory', value: 'Upto 32GB' },
+            { label: 'Phonebook', value: '500' },
+            { label: 'Message', value: '200' },
+            { label: 'Processor', value: '208 MHz' },
+        ]
+    },
+    {
+        id: 6,
+        label: 'Connectivity',
+        data: [
+            { label: 'Network Type', value: '2G' },
+            { label: 'Bluetooth Support', value: 'Yes' },
+            { label: 'Earphone Jack', value: '3.5mm' },
+            { label: 'SIM Slot', value: 'Slot 1 & Slot 2- Standard' },
+            { label: 'Dual Active/Standby', value: 'Dual Standby' },
+            { label: 'FM Radio', value: 'Wireless FM' },
+            { label: 'GSM', value: '1800/900' },
+            { label: '3G: WCDMA', value: '900/2100 MHz' },
+        ]
+    },
+    {
+        id: 7,
+        label: 'Additional',
+        data: [
+            { label: 'Languages', value: '9 (English, Hindi, Gujarati, Tamil, Telugu, Bengali, Kannada & Malayalam & Punjabi)' },
+            { label: 'Accessories', value: 'Charger & Battery' }
+        ]
+    },
+]
+
+export const FeatureRichDataKingSignal = {
+    title: 'Stay Connected Anywhere with Super Strong Signal',
+    description: `Experience unmatched connectivity with Signal Enhancement Technology, ensuring a super strong signal no matter where you are. Whether you're in remote areas, underground spaces, or high-rise buildings, stay connected without interruptions. With optimized antenna performance and advanced network stability, your calls remain crystal clear, and your messages are delivered instantly.`,
+    cards: [
+        {
+            icon: '/featurephone/common/battery-full-red.svg',
+            title: 'Type C Charger with 1500mAh Battery',
+            description: `Stay powered throughout the day without interruptions. Enjoy reliable battery life with quick and efficient charging.`
+        },
+        {
+            icon: '/featurephone/common/king-voice-red.svg',
+            title: 'Anonymous Auto call recording',
+            description: `Stay in control of your conversations, discreetly record calls without notifications, `
+        },
+        {
+            icon: '/featurephone/common/phone-red.svg',
+            title: 'Super strong and durable design',
+            description: `Whether it's accidental drops, rough handling, or extreme conditions, its rugged build ensures long-lasting performance.`
+        },
+    ]
+}
--- src/pages/feature_phone/King_signal.jsx
+++ src/pages/feature_phone/King_signal.jsx
@@ -1,13 +1,20 @@
+import FeatureRich from "../../components/common/feature_phone_common/FeatureRich";
+import PhoneSliderAnimation from "../../components/common/feature_phone_common/PhoneSliderAnimation";
+import StickyHeader from "../../components/common/StickyHeader";
 import King_signal_section1 from "../../components/core/feature_phone/king_signal/Section1";
 import Silverplated from "../../components/core/feature_phone/king_signal/Silverplated";
 import Strong_Signal from "../../components/core/feature_phone/king_signal/strong_signal";
+import { FeatureRichDataKingSignal, HeroDataKingSignal } from "../../data/featurephonedata/KingSignal";
 
-function King_signal(){
+function King_signal() {
     return (
         <>
-            <King_signal_section1/>
-            <Strong_Signal/>
-            <Silverplated/>
+            <StickyHeader {...HeroDataKingSignal} />
+            <FeatureRich {...FeatureRichDataKingSignal} />
+            <PhoneSliderAnimation />
+            <King_signal_section1 />
+            <Strong_Signal />
+            <Silverplated />
         </>
     )
 }
